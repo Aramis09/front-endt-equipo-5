f84cd0f626fc9d040da2bad857d6539412ab1867
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-03-13 17:40:08 -0500

    change vite config

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: { 
     port: 3000
-  }
+  },
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react-router-dom'
+    ]
+  },
 })
